8d10c2709910c992055368a7761dd105de34ea6e
skyeBreach <skyebenson@hotmail.com>
2026-06-11 03:39:26 +0100

    feat(neovim): add coding plugin config

---
+++ nvim/lua/plugins/coding.lua
@@ -0,0 +1,14 @@
+return {
+
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "lewis6991/async.nvim",
+        },
+        lazy = false,
+    },
+    {
+        "wakatime/vim-wakatime",
+        lazy = false,
+    },
+}
